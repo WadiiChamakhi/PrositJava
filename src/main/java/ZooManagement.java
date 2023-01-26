779de1f561e0088b12cc6c34c8e81301ed99ceda
WadiiChamakhi <wadii.chamakhi@esprit.tn>
2023-01-26 20:34:58 +0100

    Instruction 1

---
+++ src/main/java/ZooManagement.java
@@ -0,0 +1,11 @@
+
+public class ZooManagement {
+
+	public static void main(String[] args) {
+		int nbrCage = 20;
+		String zooName = "my zoo";
+		System.out.println(zooName+" comporte "+nbrCage+" cages");
+		
+	}
+
+}
